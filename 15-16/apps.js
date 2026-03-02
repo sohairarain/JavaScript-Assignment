bbf3da7dd63346560cb85cbd84d0120c57af1a2b
Sohair Nasir <sohairarain002@gmail.com>
2026-03-02 11:54:37 +0500

    15 - 16 Chapter

---
+++ 15-16/apps.js
@@ -0,0 +1,95 @@
+// var arr = []
+// var Constructor = new Array()
+// var student =  ["sohair","ALi"]
+// var Num  = [1 , 2]
+// var boolean = [true , false]
+// var mixed  = [Num[1] , student[0] , boolen[0]]
+
+// console.log(mixed)
+
+// var arr = ["Hsc","Bsc","Ssc","Bs","Bcom","Ms","M.Phil","Phd"]
+
+// for(var i = 1 ; i < arr.length ; i++){
+
+//     document.write(i + " )" + arr[i] +'</br>' )
+// }
+
+// var studentName = ["Sohair" , "Ali" , "Muneeb"]
+// var score = [300 , 459 ,287]
+
+// for(var a = 0 ; a < studentName.length ; a++){
+
+//     var calu = score[a] / 500 *(100)
+//     var show = "score of " + studentName[a] + " is " + score[a] +" percentage: " + calu     
+//     document.write(show+"</br></br>")
+// }
+
+// var userFirstColor = prompt("Enter color Name ot add in array start")
+// var Arr = ["blue","green","white"]
+// var StartAdd = Arr.unshift(userFirstColor ,userFirstColor ) 
+// alert(Arr)
+
+// var userendColor = prompt("Enter color Name ot add in array end")
+// var EndAdd = Arr.push(userendColor)
+// alert(Arr)
+
+// var userDelStart = +prompt("how to Delete array list in color start")
+// var userFirstDel = Arr.shift()
+
+// alert(Arr)
+
+
+// var userDelEnd = +prompt("how to Delete array list in color end")
+// var userFirstDel = Arr.pop()
+
+// alert(Arr)
+
+// var userSpecialNum    = prompt("apko ya color khidr add kar na hai ya batio ")
+// var userSpicfickColor = prompt("Apko konsa color add karna us number par")
+// var addspecialNum  = Arr.splice( +userSpecialNum, 0, userSpicfickColor)
+// alert(Arr)
+
+// var userSpecialDel = prompt("ap Kon sa index delete kar wana cha rahy ho")
+// var userIndexDelStart = prompt("apko kitny index delete karny hai")
+// var del = Arr.splice(+userSpecialDel , +userIndexDelStart)
+// alert(Arr)
+
+// document.write(`Score of Students : ${score} </br>`)
+// document.write(`Ordered Scores of Students : ${score.sort()}</br>`)
+
+// var cities = ["Karachi","lahore","Islamabad","Quetta","peshawar","Multan","Sakhar"]
+// document.write(`Cities List : </br> ${cities}</br></br>`)
+
+// document.write(`Selected cities: </br> ${cities.slice(2,5)}</br>`)
+
+// var text = ["This","is","my","car"]
+// document.write("Array : </br>" + text+"</br>")
+// document.write("String : </br>"+text.join(" ")+"</br>")
+
+
+// var FIFO = ["mouse","proccesser","Keyboard","Ram"]
+
+// for(i = FIFO.length  ; i > 0 ; i--){
+
+//     console.log("Out </br>"+FIFO.pop()+"</br>")
+    
+// }
+
+
+// var FOFI = ["mouse","proccesser","Keyboard","Ram"]
+
+
+// for(var b = FOFI.length ; b > 0  ; b--){
+    
+//    console.log("Out</br>"+FOFI.shift()+"</br>")
+   
+
+// }
+
+// var arr = ["Apple","Samsung","Motorola","Nokia","Sony"]
+// document.write("<select>")
+// for(i = 0 ; i < arr.length ; i++){
+//     document.write(`<option> ${arr[i]}</option>`)
+// }
+// document.write("<select>")
+
