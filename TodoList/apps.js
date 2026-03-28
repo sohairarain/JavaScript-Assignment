29b3a20f82e8651db4893cdbc62c94a34b1a45b8
Sohair Nasir <sohairarain002@gmail.com>
2026-03-28 13:11:43 +0500

    TodoList input to input

--- TodoList/apps.js
+++ TodoList/apps.js
@@ -1,23 +1,38 @@
-var arr = []
+var arr = JSON.parse(localStorage.getItem("todoList")) || [];
 
-function userInput(){
-    var userData = document.getElementById('user-input')
-    arr.push(userData.value+"  RS10")
-    userData.value =""
-print()
+console.log(arr);
+var userData = document.getElementById("user-input");
+var subButton = document.getElementById("sub-btn");
+
+function userInput() {
+  arr.push(userData.value);
+  localStorage.setItem("todoList", JSON.stringify(arr));
+  userData.value = "";
+  subButton.innerHTML = "submit";
+  print();
 }
 
-function print(){
-    var con = document.getElementById('container')
-    con.innerHTML=""
+function print() {
+  var con = document.getElementById("container");
+  con.innerHTML = "";
 
- for(var i = 0; i < arr.length; i++){
-   
-    con.innerHTML +=`<li>${arr[i]}<button onclick="remove(${i})">delete${i}</button></li>`
+  for (var i = 0; i < arr.length; i++) {
+    con.innerHTML += `<li>${arr[i]}<div><button onclick="remove(${i})">delete</button><button onclick="Edit(${i})">Edit</button></div></li>`;
+  }
 }
+
+function Edit(Enum) {
+  var cutLi = arr.splice(Enum, 1);
+  userData.value = cutLi;
+  //   var replace = arr.splice(Enum , 0 ,cutLi)
+  subButton.innerHTML = "update";
+  console.log(replace);
+  print();
 }
-function remove(index){
-    var delet = arr.splice(index,1)
-    console.log(index)
-    print()    
+
+function remove(index) {
+  var delet = arr.splice(index, 1);
+  console.log(index);
+  localStorage.setItem("todoList", JSON.stringify(arr));
+  print();
 }
